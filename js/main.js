c98664af40cb026b76208dab78e437689e614af6
Tasty63 <57009535+Tasty63@users.noreply.github.com>
2020-06-05 20:48:09 +0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,7 @@
+
+const menuBtn = document.querySelector('.m-menu');
+const menuSidebar = document.querySelector('.menu-sidebar');
+
+menuBtn.addEventListener('click', () => {
+    menuSidebar.classList.toggle('active');
+})
